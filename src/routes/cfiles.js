cbb83001c9a7f9a61b9184468127c24bde3ed203
Fikasentani <mpilo.dlamini@yahoo.com>
2020-09-08 19:55:47 +0200

    adding multer for file upload to patch route

--- src/routes/cfiles.js
+++ src/routes/cfiles.js
@@ -4,8 +4,33 @@ const CfileController = require('../controllers/cfiles');
 
 const router = express.Router();
 
+
+const multer = require("multer");
+
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+};
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype]; //if the mime type is not valid, it returns null
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "uploads/images")
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLocaleLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, `${name}-${Date.now()}.${ext}`);
+  }
+});
+
 router.post("", CfileController.createCfile);
-router.patch("/new/consultation/:id", CfileController.createConsultation);
+router.patch("/new/consultation/:id", multer(storage).single("image"), CfileController.createConsultation);
 router.patch("/:id", CfileController.updateCfile);
 router.patch("/update/consultation/:id", CfileController.updateConsultation);
 router.get("", CfileController.getCfiles);
